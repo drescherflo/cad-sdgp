63bda0c6d55ee011d2bd30e3d87a4addfbfffc20
drescherflo <drescherflo@protonmail.com>
2023-12-27 15:18:14 +0100

    Modify 5 isaac cubes to be randomized with replicator

--- custom_train_data/experiments_playground.py
+++ custom_train_data/experiments_playground.py
@@ -30,6 +30,7 @@ from omni.isaac.core.utils.nucleus import get_assets_root_path
 from omni.isaac.core.utils.rotations import euler_angles_to_quat
 from omni.isaac.core.utils.stage import get_current_stage, create_new_stage
 from omni.isaac.core.objects import DynamicCuboid
+from omni.isaac.core.utils.semantics import add_update_semantics
 
 
 
@@ -42,8 +43,30 @@ def register_random_replicator_cubes():
             rep.modify.pose(position=rep.distribution.uniform((-1, -1, 0.5), (1, 1, 0.5)))
         return cubes_grp.node
 
-    # Register
+    # Register randomization function
     rep.randomizer.register(place_replicator_cubes)
+
+
+def register_random_isaac_sim_cubes(scene):
+    # Create cubes
+    cubes = []
+    for i in range(5):
+        cubes.append(DynamicCuboid(prim_path=f"/cubes/cube{i}", name=f"cube{i}", scale=np.array([0.1, 0.1, 0.1]))) #, position=np.array([0, 0, 0.5 + i]))
+        scene.add(cubes[i])
+
+    # Apply semantics
+    for cube in cubes:
+        add_update_semantics(cube.prim, semantic_label="cube", type_label="class")
+
+    # Define randomization function
+    def place_isaac_sim_cubes():
+        cubes_as_rep_cubes = rep.get.prims(path_pattern="/cubes/cube*") #, semantics=[("class", "cube")])
+        with cubes_as_rep_cubes:
+            rep.modify.pose(position=rep.distribution.uniform((-1, -1, 0.5), (1, 1, 0.5)))
+        return cubes_as_rep_cubes
+
+    # Register randomization function
+    rep.randomizer.register(place_isaac_sim_cubes)
 
 
 # Run simulation
@@ -66,18 +89,19 @@ def main():
 
     ### Add cubes from replicator with registration function and semantic labels and randomized position
     ## Register replicator graph nodes
-    register_random_replicator_cubes()
+    #register_random_replicator_cubes()
 
     ## Register randomizer functions to run on every frame
-    with rep.trigger.on_frame():
-        rep.randomizer.place_replicator_cubes()
+    #with rep.trigger.on_frame():
+    #    rep.randomizer.place_replicator_cubes()
 
 
     ### Add 5 cubes with physics (so called cuboids) from isaac core objects
-    # cubes = []
-    # for i in range(5):
-    #     cubes.append(DynamicCuboid(prim_path=f"/cubes/cube{i}", name=f"cube{i}", position=np.array([0, 0, 0.5 + i]), scale=np.array([0.1, 0.1, 0.1])))
-    #     scene.add(cubes[i])
+    register_random_isaac_sim_cubes(scene)
+
+    ## Register randomizer functions to run on every frame
+    with rep.trigger.on_frame():
+        rep.randomizer.place_isaac_sim_cubes()
 
     #### Generate replicator graphs without triggering writing
     rep.orchestrator.preview()
