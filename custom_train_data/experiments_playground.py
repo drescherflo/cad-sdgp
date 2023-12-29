90f2b5ca4ba70552faa7cb18f44fe199e4770055
drescherflo <drescherflo@protonmail.com>
2023-12-29 18:09:07 +0100

    Load usd file with replicator randomization previously converted from step

--- custom_train_data/experiments_playground.py
+++ custom_train_data/experiments_playground.py
@@ -1,7 +1,9 @@
 import os
+
 print(os.environ["CARB_APP_PATH"])
 print(os.environ["ISAAC_PATH"])
 print(os.environ["EXP_PATH"])
+print(os.getcwd())
 
 # Launch Isaac Sim
 import numpy as np
@@ -10,15 +12,17 @@ from omni.isaac.kit import SimulationApp
 CONFIG = {"renderer": "RayTracedLighting", "headless": False}
 simulation_app = SimulationApp(launch_config=CONFIG)
 
-
 # Register signal handler for Ctrl+C for exiting
 import signal
+
+
 def signal_handler(signal, frame):
     print("Received Ctrl+C, exiting...")
     simulation_app.close()
     exit(0)
-signal.signal(signal.SIGINT, signal_handler)
 
+
+signal.signal(signal.SIGINT, signal_handler)
 
 # Main code starts here
 # All omniverse imports need to be done after the simulation has been created (https://docs.omniverse.nvidia.com/isaacsim/latest/replicator_tutorials/tutorial_replicator_offline_generation.html#running-as-a-simulationapp 20.12.2023)
@@ -31,7 +35,6 @@ from omni.isaac.core.utils.rotations import euler_angles_to_quat
 from omni.isaac.core.utils.stage import get_current_stage, create_new_stage
 from omni.isaac.core.objects import DynamicCuboid
 from omni.isaac.core.utils.semantics import add_update_semantics
-
 
 
 def register_random_replicator_cubes():
@@ -51,7 +54,8 @@ def register_random_isaac_sim_cubes(scene):
     # Create cubes
     cubes = []
     for i in range(5):
-        cubes.append(DynamicCuboid(prim_path=f"/cubes/cube{i}", name=f"cube{i}", scale=np.array([0.1, 0.1, 0.1]))) #, position=np.array([0, 0, 0.5 + i]))
+        cubes.append(DynamicCuboid(prim_path=f"/cubes/cube{i}", name=f"cube{i}",
+                                   scale=np.array([0.1, 0.1, 0.1])))  # , position=np.array([0, 0, 0.5 + i]))
         scene.add(cubes[i])
 
     # Apply semantics
@@ -60,7 +64,7 @@ def register_random_isaac_sim_cubes(scene):
 
     # Define randomization function
     def place_isaac_sim_cubes():
-        cubes_as_rep_cubes = rep.get.prims(path_pattern="/cubes/cube*") #, semantics=[("class", "cube")])
+        cubes_as_rep_cubes = rep.get.prims(path_pattern="/cubes/cube*")  # , semantics=[("class", "cube")])
         with cubes_as_rep_cubes:
             rep.modify.pose(position=rep.distribution.uniform((-1, -1, 0.5), (1, 1, 0.5)))
         return cubes_as_rep_cubes
@@ -70,6 +74,19 @@ def register_random_isaac_sim_cubes(scene):
 
 
 # Run simulation
+def register_random_usd_cad_model():
+    def place_replicator_models():
+        # Load cad models with replicator
+        models = rep.create.from_usd(usd="CAD Models/STL_converted/MA Simple Object_stl.usd", semantics=[("class", "simple_object")], count=2)
+        with models:
+            rep.modify.pose(position=rep.distribution.uniform((-1, -1, 0.5), (1, 1, 0.5)))
+            rep.modify._scale(scale=(0.001, 0.001, 0.001))  # Set correct scale (unit of step file is mm, but isaac sim uses m)
+        return models
+
+    # Register randomization function
+    rep.randomizer.register(place_replicator_models)
+
+
 def main():
     #### Setup simulation
     ### Create new stage
@@ -82,50 +99,68 @@ def main():
     ### Add ground plane to scene
     scene.add_default_ground_plane()
 
+    ### Rerender simulation
+    simulation_app.update()
+
     #### Code snippets
+
+    ####################################################################
     ### Create 100 cones with replicator
-    #rep.create.cone(count=100, position=rep.distribution.uniform((-100, -100, -100), (100, 100, 100)), semantics=[("class", "cone")])
+    # rep.create.cone(count=100, position=rep.distribution.uniform((-100, -100, -100), (100, 100, 100)), semantics=[("class", "cone")])
 
-
+    ####################################################################
     ### Add cubes from replicator with registration function and semantic labels and randomized position
     ## Register replicator graph nodes
-    #register_random_replicator_cubes()
+    # register_random_replicator_cubes()
 
     ## Register randomizer functions to run on every frame
-    #with rep.trigger.on_frame():
+    # with rep.trigger.on_frame():
     #    rep.randomizer.place_replicator_cubes()
 
-
+    ####################################################################
     ### Add 5 cubes with physics (so called cuboids) from isaac core objects
-    register_random_isaac_sim_cubes(scene)
+    # register_random_isaac_sim_cubes(scene)
+
+    ## Register randomizer functions to run on every frame
+    # with rep.trigger.on_frame():
+    #     rep.randomizer.place_isaac_sim_cubes()
+
+    ####################################################################
+    ### Add 2 CAD Models with replicator with semantic labels and randomized position
+    register_random_usd_cad_model()
 
     ## Register randomizer functions to run on every frame
     with rep.trigger.on_frame():
-        rep.randomizer.place_isaac_sim_cubes()
+        rep.randomizer.place_replicator_models()
 
+    ####################################################################
     #### Generate replicator graphs without triggering writing
     rep.orchestrator.preview()
 
+    ####################################################################
     #### Run replicator for 100 frames
-    #rep.orchestrator.run(num_frames=100)
+    # rep.orchestrator.run(num_frames=100)
 
+    ####################################################################
     #### Run replicator for one step/frame
-    #rep.orchestrator.step()
+    # rep.orchestrator.step()
 
+    ####################################################################
     #### Run (Physics) Simulation for 100000 steps
     # for i in range(100000):
     #    world.step(render=True, step_sim=True)
 
+    ####################################################################
     #### Replicator run-loop (only for testing, do NOT use this when generating data)
     while (True):
         rep.orchestrator.step()
 
+    ####################################################################
     #### Isaac Sim run-loop (only for testing, do NOT use this when generating data)
-    #while (True):
-    #    simulation_app.update()
+    while (True):
+       simulation_app.update()
 
 
 if __name__ == "__main__":
     main()
     simulation_app.close()
-
