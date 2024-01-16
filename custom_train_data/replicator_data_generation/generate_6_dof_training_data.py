371ced3b9bd278c9c8e00455bd2e7b32a485030e
drescherflo <drescherflo@protonmail.com>
2024-01-16 17:49:04 +0100

    Working randomization of dome light color and sphere lights from config

---
+++ custom_train_data/replicator_data_generation/generate_6_dof_training_data.py
@@ -0,0 +1,165 @@
+# Regular imports
+import argparse
+import os
+import json
+import sys
+import numpy as np
+
+# Parse args
+parser = argparse.ArgumentParser()
+args = parser.parse_args(sys.argv[1:])
+
+# TODO parse headless
+# TODO parse out_dir
+# TODO parse usd_dir
+# TODO parse conf_path
+
+
+# Launch Isaac Sim
+from omni.isaac.kit import SimulationApp
+
+CONFIG = {"renderer": "RayTracedLighting", "headless": False}
+simulation_app = SimulationApp(launch_config=CONFIG)
+
+
+# Omniverse imports
+import omni
+import omni.replicator.core as rep
+import omni.graph.core as og
+from omni.isaac.core.utils.stage import create_new_stage
+from omni.isaac.core import World, SimulationContext
+from omni.isaac.core.prims import XFormPrim
+from omni.isaac.core.utils import prims, extensions
+from omni.isaac.core.utils.semantics import add_update_semantics
+from omni.isaac.core.materials import OmniPBR, OmniGlass
+from omni.isaac.sensor import Camera
+from omni.isaac.core.utils.rotations import euler_angles_to_quat
+
+
+def generate_materials(materials_config) -> list[OmniGlass | OmniPBR]:
+    materials = []
+    for material_config in materials_config:
+        if material_config["is_glass"]:
+            materials.append(OmniGlass(f"/obj_materials/material_{material_config['material_idx']}", color=np.array(material_config["color"])))
+        else:
+            material = OmniPBR(f"/obj_materials/material_{material_config['material_idx']}", color=np.array(material_config["color"]))
+            material.set_reflection_roughness(material_config["surface_roughness"])
+            materials.append(material)
+
+    return materials
+
+
+def extract_per_scene_config(scenes: list[dict], config_key: str) -> list:
+    return [scene[config_key] for scene in scenes]
+
+
+def main(conf_path: str, usd_dir: str, out_dir: str) -> None:
+    # Load json config
+    with open(conf_path, "r") as f:
+        config = json.load(f)
+
+    # Parse general config
+    image_width = config["camera_config"]["frame_width"]
+    image_height = config["camera_config"]["frame_height"]
+    camera_position = config["camera_config"]["pose"]["position"]
+    camera_orientation = config["camera_config"]["pose"]["orientation"]
+    sub_frames_per_frame = config["sub_frames_per_frame"]
+    usd_models = config["usd_models"]
+    writer_configs = config["writer_configs"]
+    num_frames_per_object = config["num_frames_per_object"]
+
+    # Scene generation loop
+    for object_type_specific_scene_configs in config["scenes"]:
+        # Parse scene configs
+        background_colors = extract_per_scene_config(object_type_specific_scene_configs, "background_color")
+        dome_light_colors = extract_per_scene_config(object_type_specific_scene_configs, "dome_light_color")
+        sphere_light_configs = extract_per_scene_config(object_type_specific_scene_configs, "sphere_light_configs")
+        num_sphere_lights = len(sphere_light_configs[0])
+        object_configs = extract_per_scene_config(object_type_specific_scene_configs, "object_configs")
+        num_objects_per_scene = len(object_configs[0])
+
+        # Reset simulation
+        # We need to reset every num_frames_per_object because the usd models change after num_frames_per_object
+        create_new_stage()  # Create new stage and delete previous contents
+        world = World()  # Get world
+
+        # Generate object materials
+        materials = generate_materials(config["materials"])
+        #background_material = OmniPBR("materials/background")
+
+        # Add camera
+        camera = rep.create.camera(position=camera_position, rotation=camera_orientation)
+        render_product = rep.create.render_product(camera=camera, resolution=(image_width, image_height))
+
+        # Initialize writers
+        writers = []
+        for writer_config in writer_configs:
+            writer = rep.WriterRegistry.get(writer_config["name"])
+            writer.initialize(output_dir=out_dir, **writer_config["args"])
+            writer.attach(render_product)
+            writers.append(writer)
+
+        # Add dome light
+        dome_light = rep.create.light(light_type="Dome")
+
+        # Add sphere_lights
+        sphere_lights = [rep.create.light(light_type="Sphere") for _ in range(num_sphere_lights)]
+
+        # Add ground plane to scene as background
+        plane_material = rep.create.material_omnipbr(roughness=1)
+        plane = rep.create.plane(scale=10, visible=True, material=plane_material)
+
+        # Add objects
+        object_prims = []
+        for i, object_config in enumerate(object_configs[0]):
+            usd_path = os.path.join(usd_dir, object_config["usd_model"])
+            object_prims.append(prims.create_prim(prim_path=f"/objects/object_{i}", usd_path=usd_path, semantic_label=object_config["semantic_class_label"]))
+
+        # Configure replicator "randomization"
+        def randomize_sphere_light(sphere_lights, sphere_light_idx, sphere_light_configs):
+            sphere_light = sphere_lights[sphere_light_idx]
+            sphere_light_config = [sphere_light_per_frame_config[sphere_light_idx] for sphere_light_per_frame_config in sphere_light_configs]
+            sphere_light_positions = [config["position"] for config in sphere_light_config]
+            sphere_light_colors = [config["color"] for config in sphere_light_config]
+            with sphere_light:
+                rep.modify.attribute("color", rep.distribution.sequence(sphere_light_colors))
+                rep.modify.pose(position=rep.distribution.sequence(sphere_light_positions))
+            return sphere_light
+
+        rep.randomizer.register(randomize_sphere_light)
+
+        with rep.trigger.on_frame():  # Change on every rendered frame
+            # Change background color
+            with plane_material:
+                rep.modify.attribute("color", rep.distribution.sequence(background_colors))
+
+            # Change dome_light color
+            with dome_light:
+                rep.modify.attribute("color", rep.distribution.sequence(dome_light_colors))
+
+            # Change sphere light color and position
+            for i in range(len(sphere_lights)):
+                rep.randomizer.randomize_sphere_light(sphere_lights, i, sphere_light_configs)
+
+        # TODO: remove following 5 lines
+        # Isaac Sim run-loop (only for testing and ROS publishing, do NOT use this when generating data)
+        rep.orchestrator.preview()
+        world.reset()  # This is required instead of sim_app.update for ros publishers to work
+        while simulation_app.is_running():
+            world.step(render=True)  # This is required instead of sim_app.update for ros publishers to work
+        
+        # Capture training data
+        num_scenes = len(object_type_specific_scene_configs)
+        for scene_nr in range(num_scenes):
+            print(f"Writing frame {str(scene_nr + 1)} of {num_scenes}")
+
+            # Generate 32 subframes for 1 frame for better quality (see https://docs.omniverse.nvidia.com/extensions/latest/ext_replicator/subframes_examples.html#subframes-examples (08.01.2024))
+            rep.orchestrator.step(rt_subframes=32)
+
+        
+
+if __name__ == '__main__':
+    conf_path = "custom_train_data/replicator_data_generation/generated_configs/6_dof_only_simple_object.json"
+    usd_dir = "custom_train_data/replicator_data_generation/generated_configs/6_dof_only_simple_object.json"
+    out_dir = "temp_replicator_out"
+    main(conf_path, usd_dir, out_dir)
