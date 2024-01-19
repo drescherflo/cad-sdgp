13ab8cb9d850f3f29e7bbf948e3bf7f0174de5e0
drescherflo <drescherflo@protonmail.com>
2024-01-19 14:12:57 +0100

    Generate new 6 dof config

--- custom_train_data/replicator_data_generation/config_generation/generate_6_dof_conf.sh
+++ custom_train_data/replicator_data_generation/config_generation/generate_6_dof_conf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 data_generation_config_generator.py --usd_dir "../../../CAD Models/OBJ_converted" --writer ResumableBasicWriter rgb=True camera_params=True --writer WorldPoseWriter --out_path generated_configs/6_dof_only_simple_object.json --num_frames_per_object 10
+python3 data_generation_config_generator.py --usd_dir "../../../CAD Models/OBJ_converted" --writer ResumableBasicWriter rgb=True camera_params=True --writer WorldPoseWriter --out_path generated_configs/6_dof_only_simple_object.json --num_frames_per_object 1000
