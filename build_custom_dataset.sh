0951150c1ba966d2c6469edd7e658d730166ece1
drescherflo <drescherflo@protonmail.com>
2024-01-25 00:53:55 +0100

    - Add dataset_converter - Remove cleanup

--- build_custom_dataset.sh
+++ build_custom_dataset.sh
@@ -8,6 +8,4 @@ docker compose run step_to_obj_converter
 docker compose run obj_to_usd_converter
 docker compose run config_generator_for_6_dof_dataset_generation
 docker compose run 6_dof_dataset_generator
-
-# Clean up
-docker compose down
+docker compose run dataset_converter
