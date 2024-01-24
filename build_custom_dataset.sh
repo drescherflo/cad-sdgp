36de04db92032f737d123baaa37079344bb452a1
drescherflo <drescherflo@protonmail.com>
2024-01-25 00:24:57 +0100

    Enable bash strict mode

--- build_custom_dataset.sh
+++ build_custom_dataset.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Enable strict mode.
+set -euo pipefail
+
+# Run dataset generation
 docker compose run step_to_obj_converter
 docker compose run obj_to_usd_converter
 docker compose run config_generator_for_6_dof_dataset_generation
 docker compose run 6_dof_dataset_generator
+
+# Clean up
 docker compose down
