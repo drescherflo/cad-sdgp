cf124498d94df74db25d5ab2352fad768ca3b40d
drescherflo <drescherflo@protonmail.com>
2024-01-25 00:07:48 +0100

    - Add service for 6_dof_dataset generation - Fix wrong args for ResumableWriter - Rename config generator service

--- build_custom_dataset.sh
+++ build_custom_dataset.sh
@@ -2,4 +2,6 @@
 
 docker compose run step_to_obj_converter
 docker compose run obj_to_usd_converter
-docker compose run config_generator_for_dataset_generation
+docker compose run config_generator_for_6_dof_dataset_generation
+docker compose run 6_dof_dataset_generator
+docker compose down
