8515006402ac5f99b18f028ebf72dd70b2c14586
drescherflo <drescherflo@protonmail.com>
2024-01-24 01:04:07 +0100

    - Introduce environment.yaml - Dockerize step to obj converter

--- step_to_usd_conversion/setup.sh
+++ step_to_usd_conversion/setup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
+# Warning! This script must not be used in the dockerfile to create the image, because conda activate cannot be used!
 
-# Create and activate conda environment
-yes | conda create -n step_to_usd python=3.10
-conda activate step_to_usd
-
-# Install pythonocc-core
-yes | conda update --all
-yes | conda install -c conda-forge pythonocc-core=7.7.2
-
-# Install stl-obj-convertor
-yes | pip install --upgrade pip
-yes | pip install numpy-stl
+yes | conda env create -f environment.yaml
