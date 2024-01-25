a3f679b26b290f28f7bbc17098da91710dfad3dd
drescherflo <drescherflo@protonmail.com>
2024-01-25 01:50:44 +0100

    Allow multiple replicator dataset dirs

--- custom_train_data/dataset_conversion/dataset_converter.py
+++ custom_train_data/dataset_conversion/dataset_converter.py
@@ -24,14 +24,16 @@ def main(args: list[str]) -> None:
     # Create argument parser
     parser = argparse.ArgumentParser(description="Converts the generated training data from NVIDIA Replicator to other formats using the plugins in the converter_plugins directory")
     parser.add_argument("--obj_dir", help="Directory with all CAD Models in OBJ format", required=True)
-    parser.add_argument("--replicator_data_dir", help="Input directory containing the files in the Replicator format", required=True)
+    parser.add_argument("--replicator_data_dir", action="append", help="Input directory containing the files in the Replicator format. Argument can be added multiple times", required=True)
     parser.add_argument("--output_dir", help="Directory in which the plugins should create the converted data. Each plugin gets its own subdirectory", required=True)
     args = parser.parse_args(args)
 
-    # Test if replicator_data_dir and obj_dir exist
-    if not os.path.isdir(args.replicator_data_dir):
-        print(f"The NVIDIA replicator directory {args.rep_dir} does not exist. Existing...")
-        exit(-1)
+    # Test if replicator_data_dirs and obj_dir exist
+    replicator_dataset_dirs = args.replicator_data_dir
+    for replicator_dataset_dir in replicator_dataset_dirs:
+        if not os.path.isdir(replicator_dataset_dir):
+            print(f"The NVIDIA replicator directory {args.rep_dir} does not exist. Existing...")
+            exit(-1)
     if not os.path.isdir(args.obj_dir):
         print(f"The OBJ model directory {args.rep_dir} does not exist. Existing...")
         exit(-1)
@@ -43,16 +45,26 @@ def main(args: list[str]) -> None:
                               plugin_package_name)  # plugin_dir has to be relative to the script. Depending on the execution method, this is not always the case
     converter_plugins = load_converter_plugins(plugin_dir, plugin_package_name)
 
-    # Run conversion process for each converter plugin
-    for converter_plugin in converter_plugins:
-        print("Converting with plugin", converter_plugin.__name__, "...")
+    # Convert every dataset
+    for replicator_dataset_dir in replicator_dataset_dirs:
+        # Get base dir for building correct output dir
+        base_dir = os.path.basename(replicator_dataset_dir)
 
-        # Create plugin output dir
-        plugin_out_dir = os.path.join(args.output_dir, str(converter_plugin.__name__))
-        os.makedirs(plugin_out_dir, exist_ok=True)
+        # Run conversion process for each converter plugin
+        for converter_plugin in converter_plugins:
+            print("Converting with plugin", converter_plugin.__name__, "...")
 
-        # Convert data with plugin
-        converter_plugin.convert(args.replicator_data_dir, args.obj_dir, plugin_out_dir)
+            # Create plugin output dir
+            plugin_out_dir = os.path.join(args.output_dir, base_dir, converter_plugin.__name__)
+            os.makedirs(plugin_out_dir, exist_ok=True)
+
+            # Check for empty output dir
+            if len(os.listdir(plugin_out_dir)) != 0:
+                print(f"Output directory {plugin_out_dir} is not empty. Exiting...")
+                exit(-1)
+
+            # Convert data with plugin
+            converter_plugin.convert(replicator_dataset_dir, args.obj_dir, plugin_out_dir)
 
 
 if __name__ == '__main__':
