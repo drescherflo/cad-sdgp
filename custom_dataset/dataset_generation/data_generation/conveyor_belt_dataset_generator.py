11dd1fc37d7f55236e17839546a77d88d46f8718
drescherflo <drescherflo@protonmail.com>
2024-02-09 14:24:11 +0100

    Working parsing and applying of config file

--- custom_dataset/dataset_generation/data_generation/conveyor_belt_dataset_generator.py
+++ custom_dataset/dataset_generation/data_generation/conveyor_belt_dataset_generator.py
@@ -16,7 +16,7 @@ simulation_app = SimulationApp(launch_config=CONFIG)
 import omni.replicator.core as rep
 from omni.isaac.core import World
 from omni.isaac.core.utils import extensions
-from omni.isaac.core.utils.stage import create_new_stage, open_stage
+from omni.isaac.core.utils.stage import close_stage, open_stage, is_stage_loading
 from omni.isaac.core.utils import prims
 from omni.isaac.core.utils.rotations import euler_angles_to_quat
 from omni.isaac.core.prims import RigidPrim, XFormPrim, GeometryPrim
@@ -59,6 +59,7 @@ def main(conf_path: str, usd_dir: str, out_dir: str) -> None:
     sub_frames_per_frame = config["sub_frames_per_frame"]
     conveyor_belt_speed = config["conveyor_belt_speed"]
     min_x_pos_for_record_start = config["min_x_pos_for_record_start"]
+    num_frames_per_scene = config["num_frames_per_scene"]
 
     # Parse writer config
     # writer_configs = parse_writer_args(args.writer)  TODO uncomment
@@ -88,20 +89,50 @@ def main(conf_path: str, usd_dir: str, out_dir: str) -> None:
 
     # Scene generation loop
     frame_number = 0
-    num_frames = len(config["scenes"]) * config["num_frames_per_scene"]
+    num_frames = len(config["scenes"]) * num_frames_per_scene
     for scene_nr, scene_config in enumerate(config["scenes"]):
         print("Setting up scene", scene_nr + 1, "of", len(config["scenes"]))
 
-        # Reset simulation
-        create_new_stage()  # Create new stage and delete previous contents
+        # Parse per frame config
+        per_frame_config = scene_config["per_frame_config"]
+        camera_positions = [pose["position"] for pose in per_frame_config["camera_poses"]]
+        camera_orientations = [pose["rotation"] for pose in per_frame_config["camera_poses"]]
+        sphere_light_configs = per_frame_config["sphere_light_configs"]
+        num_sphere_lights = len(sphere_light_configs[0])
+        distant_light_orientations = [conf["rotation"] for conf in per_frame_config["distant_light_configs"]]
+        distant_light_colors = [conf["color"] for conf in per_frame_config["distant_light_configs"]]
+        distant_light_intensities = [conf["intensity"] for conf in per_frame_config["distant_light_configs"]]
+        ground_plane_colors = per_frame_config["ground_plane_colors"]
+        object_material_assignments = per_frame_config["object_material_assignments"]
         
-        # Load stage
+        # Reset simulation by loading stage
         if not open_stage(os.path.join(os.path.dirname(os.path.abspath(__file__)), "isaac_worlds/conveyor.usd")):
-            print("Could not open world. Exiting...", file=sys.stderr)
+            print("Could not load conveyor world. Exiting...", file=sys.stderr)
             quit_on_error(simulation_app)
 
         # Get world
         world = World()
+
+        # Generate object materials
+        materials = generate_materials(config["materials"])
+
+        # Add camera
+        camera = rep.create.camera()
+        render_product = rep.create.render_product(camera=camera, resolution=(image_width, image_height))
+
+        # Initialize writers
+        # writers = []
+        # for writer_config in writer_configs:
+        #     writer = rep.WriterRegistry.get(writer_config["name"])
+        #     writer.initialize(output_dir=out_dir, init_frame_nr=frame_number, **writer_config["args"])
+        #     writer.attach(render_product)
+        #     writers.append(writer)
+
+        # Add distant light
+        distant_light = rep.create.light(light_type="distant")
+
+        # Add sphere lights
+        sphere_lights = [rep.create.light(light_type="sphere") for _ in range(num_sphere_lights)]
 
         # Create scene objects
         num_objects_per_scene = len(scene_config["objects"])
@@ -160,8 +191,68 @@ def main(conf_path: str, usd_dir: str, out_dir: str) -> None:
             min_x_pos_for_record_start_passed = any(
                 [np.linalg.norm(object_prim.get_world_pose()[0][0]) > min_x_pos_for_record_start for object_prim in object_rigid_prims])
 
-        while simulation_app.is_running():
-            simulation_app.update()
+        # Configure replicator "randomization"
+        def randomize_sphere_light(sphere_lights, sphere_light_idx, sphere_light_configs):
+            sphere_light = sphere_lights[sphere_light_idx]
+            sphere_light_config = [sphere_light_per_frame_config[sphere_light_idx] for sphere_light_per_frame_config in sphere_light_configs]
+            sphere_light_positions = [config["position"] for config in sphere_light_config]
+            sphere_light_colors = [config["color"] for config in sphere_light_config]
+            sphere_light_intensities = [config["intensity"] if "intensity" in config else 1000 for config in sphere_light_config]  # if expression required for compatibility with older configs. 1000 is default value according to https://docs.omniverse.nvidia.com/py/replicator/1.10.10/source/extensions/omni.replicator.core/docs/API.html#omni.replicator.core.create.light (08.02.2024)
+            with sphere_light:
+                rep.modify.attribute("color", rep.distribution.sequence(sphere_light_colors))
+                rep.modify.attribute("intensity", rep.distribution.sequence(sphere_light_intensities))
+                rep.modify.pose(position=rep.distribution.sequence(sphere_light_positions))
+            return sphere_light
+
+        rep.randomizer.register(randomize_sphere_light)
+
+        with rep.trigger.on_frame():  # Change on every rendered frame
+            # Change camera position and orientation
+            with camera:
+                rep.modify.pose(position=rep.distribution.sequence(camera_positions), rotation=rep.distribution.sequence(camera_orientations))
+
+            # Change ground plane color
+            # TODO
+
+            # Change distant light color, intensity and orientation
+            with distant_light:
+                rep.modify.attribute("color", rep.distribution.sequence(distant_light_colors))
+                rep.modify.attribute("intensity", rep.distribution.sequence(distant_light_intensities))
+                rep.modify.pose(rotation=rep.distribution.sequence(distant_light_orientations))
+
+            # Change sphere light color, position and intensities
+            for i in range(len(sphere_lights)):
+                rep.randomizer.randomize_sphere_light(sphere_lights, i, sphere_light_configs)
+
+            # TODO: change conveyor belt and conveyor belt frame color
+
+        # Capture data
+        for scene_frame_nr in range(num_frames_per_scene):
+            if not simulation_app.is_running():
+                print("Simulation has been stopped. Exiting...", file=sys.stderr)
+                quit_on_error(simulation_app)
+
+            print(f"Writing scene frame {scene_frame_nr + 1} of {num_frames_per_scene} (total frame {str(frame_number + 1)} of {num_frames})")
+
+            # Assign material to object manually since replicator does not support sequential assignment
+            for obj_idx, object_prim in enumerate(object_prims):
+                material_idx = object_material_assignments[scene_frame_nr][obj_idx]["material_idx"]
+                xform_object_prim = XFormPrim(object_prim.GetPrimPath().pathString)
+                xform_object_prim.apply_visual_material(materials[material_idx])
+
+            # Run simulation for one step, but don't render, since rendering is done by replicator
+            #TODO world.step(render=False, step_sim=True)?  render False?
+
+            # Generate multiple sub-frames for 1 frame for better quality (see https://docs.omniverse.nvidia.com/extensions/latest/ext_replicator/subframes_examples.html#subframes-examples (08.01.2024))
+            rep.orchestrator.step(rt_subframes=sub_frames_per_frame)
+
+            # Increase frame_number count
+            frame_number += 1
+
+    while simulation_app.is_running():
+        simulation_app.update()
+    simulation_app.close()
+    exit(0)
     
 
 if __name__ == '__main__':
