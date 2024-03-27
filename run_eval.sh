9cb2dcafa0af2274479f2cd70ca8c4138db6cc9c
drescherflo <drescherflo@protonmail.com>
2024-03-27 19:57:52 +0100

    Add script for automatic eval data generation

---
+++ run_eval.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Enable strict mode.
+set -euo pipefail
+
+# Set variables
+CUSTOM_DATASET_DIR="$HOME/custom_dataset"
+EVAL_DATASET_BASE_DIR="$HOME/eval_dataset"
+ROCA_DIR="$HOME/Dokumente/Masterarbeit/ROCA"
+ROCA_NETWORK_DIR="$ROCA_DIR/network"
+ROCA_MODELS_AND_CONF_BASE="$CUSTOM_DATASET_DIR/ROCA_Outputs"
+
+# Create evaluation raw data
+for model_dir in "$ROCA_MODELS_AND_CONF_BASE/"*; do
+  model_dir_basename=$(basename "$model_dir")
+  echo "Creating evaluation raw data with model $model_dir_basename"
+
+  dataset_types=("cluttered" "uncluttered")
+  for dataset_type in "${dataset_types[@]}"; do
+    for eval_dataset in "$EVAL_DATASET_BASE_DIR/converted/$dataset_type"/*/ReplicatorToRocaEval; do
+      echo "Processing $eval_dataset ..."
+      conda run --no-capture-output -n roca --cwd "$ROCA_NETWORK_DIR" python "$ROCA_NETWORK_DIR/generate_evaluation_raw_data.py" \
+        --input_dir "$eval_dataset" \
+        --model "$model_dir/model_final.pth" \
+        --model_config "$model_dir/config.yaml" \
+        --data_dir "$CUSTOM_DATASET_DIR/converted/conveyor_belt/ReplicatorToRoca/Dataset" \
+        --out_file "$eval_dataset/eval_raw_data/$model_dir_basename/eval_raw_data.json" \
+        --image_out_dir "$eval_dataset/eval_images/$model_dir_basename"
+    done
+  done
+done
