1c997c9e9e807559f9614a556300cef4120a209c
drescherflo <drescherflo@protonmail.com>
2024-03-28 00:01:16 +0100

    Add empty evaluation script with notes

---
+++ evaluation/evaluation.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+# Zuerst für cluttered
+# Pro Netz jede Szene
+    # Fehler in Translation
+    # Fehler in Rotation
+    # Fehler in Objekttyp
+
+# Durchschnitt plus STD über alle Szenen pro Netz
+# Hoffentlich ergibt sich dann ein Favorit bis max 3 Favs
+
+# Durchschnitt Fehler pro Szene
+# Fehler über Frame-Verlauf
+
+# Tabelle mit Mapping, welches Objekt wurde korrekt erkannt, welches nicht
+
+# Anzahl fehlender Objekte mit Verdeckungsgrad
+
+# Dann nochmal für uncluttered
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
