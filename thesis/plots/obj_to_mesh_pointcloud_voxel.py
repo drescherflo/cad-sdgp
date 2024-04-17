35b74f9e1c0ce272dab2e1f5d0b1f1156fe72b06
drescherflo <drescherflo@protonmail.com>
2024-04-17 19:02:29 +0200

    Working visualization with whole object in image

--- thesis/plots/obj_to_mesh_pointcloud_voxel.py
+++ thesis/plots/obj_to_mesh_pointcloud_voxel.py
@@ -1,15 +1,18 @@
+# Code wurde mit Unterstützung von ChatGPT erzeugt
+
 import open3d as o3d
 import numpy as np
-
-def capture_image(window):
-    image = window.capture_screen_float_buffer(False)
-    return image
 
 def save_visualization_as_png(geometry, filename):
     # Visualisierungseinstellungen
     vis = o3d.visualization.Visualizer()
     vis.create_window(visible=False)  # Fenster nicht anzeigen
     vis.add_geometry(geometry)
+
+    ctr = vis.get_view_control()
+    ctr.set_zoom(0.9)  # Reduziere den Zoom, um mehr von der Szene zu sehen
+    #ctr.set_front([1, 0, 0])  # Richtung der Kameraansicht
+
     vis.poll_events()  # Verarbeite GUI-Events
     vis.update_renderer()  # Aktualisiere Renderer
     vis.capture_screen_image(filename)  # Bild speichern
@@ -18,17 +21,17 @@ def save_visualization_as_png(geometry, filename):
 def load_display_obj(file_path):
     # Ein OBJ-File als Mesh laden
     mesh = o3d.io.read_triangle_mesh(file_path)
+    mesh.compute_vertex_normals()
     mesh.scale(0.5, center=mesh.get_center())
     R = mesh.get_rotation_matrix_from_xyz((-np.pi / 2, 0, 0))
     mesh.rotate(R, center=(0, 0, 0))
-    mesh.compute_vertex_normals()
 
     # PNGs für jede Geometrieart speichern
-    save_visualization_as_png(mesh, "out/cone_mesh.png")
+    save_visualization_as_png(mesh, "out/02_cone_mesh.png")
     pointcloud = mesh.sample_points_poisson_disk(2000)
-    save_visualization_as_png(pointcloud, "out/cone_pointcloud.png")
+    save_visualization_as_png(pointcloud, "out/02_cone_pointcloud.png")
     voxel_grid = o3d.geometry.VoxelGrid.create_from_triangle_mesh(mesh, voxel_size=0.05)
-    save_visualization_as_png(voxel_grid, "out/cone_voxelgrid.png")
+    save_visualization_as_png(voxel_grid, "out/02_cone_voxelgrid.png")
 
 # Dateipfad der OBJ-Datei
 file_path = 'input/cone.obj'
