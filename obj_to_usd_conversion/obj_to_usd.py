ba187d44e8cefae7bf3bcd399373ff58ba5cb0ac
Florian Drescher <floriandrescher@sundk.de>
2025-08-12 13:03:30 +0200

    feat: add obj to usd converter

---
+++ obj_to_usd_conversion/obj_to_usd.py
@@ -0,0 +1,75 @@
+import argparse
+import os
+import asyncio
+
+from isaacsim import SimulationApp
+
+def get_obj_files_in_dir(directory):
+    """
+    Retrieves a list of OBJ file names from a specified directory.
+
+    :param directory: The directory to search for OBJ files.
+    :type directory: str
+    :return: A list of OBJ file names found in the directory.
+    :rtype: list
+    """
+
+    files = [f for f in os.listdir(directory) if
+             (os.path.isfile(os.path.join(directory, f)) and f.lower().endswith((".obj")))]
+    return files
+
+async def convert(input_file_path, output_file_path):    
+    import omni.kit.asset_converter
+    # https://docs.omniverse.nvidia.com/extensions/latest/ext_asset-converter.html (05.06.2025)
+    # converter_context = omni.kit.asset_converter.AssetConverterContext()  # can be used for config, we use default config
+    converter_instance = omni.kit.asset_converter.get_instance()
+    converter_task = converter_instance.create_converter_task(input_file_path, output_file_path)
+    success = await converter_task.wait_until_finished()
+    if not success:
+        detailed_status_error_string = converter_task.get_error_message()
+        print(detailed_status_error_string)
+    
+
+
+if __name__ == "__main__":
+    """
+    Main function to initiate the conversion process from OBJ to USD format.
+    Handles command-line arguments and orchestrates the conversion.
+    """
+
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Converts STEP files to STL files")
+    parser.add_argument("--input_dir", help="Input directory containing .obj files")
+    parser.add_argument("--output_dir", help="Output directory for .usd files")
+
+    # Parse args
+    args = parser.parse_args()
+    if args.input_dir is None:
+        print("No input directory provided via --input_dir. Exiting...")
+        exit(-1)
+    if args.output_dir is None:
+        print("No output directory provided via --output_dir. Exiting...")
+        exit(-1)
+    
+    # Test if input dir and output dir exist
+    if not os.path.isdir(args.input_dir):
+        print("Input directory does not exist. Exiting...")
+        exit(-1)
+    if not os.path.isdir(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    # Convert obj to usd files
+    kit = SimulationApp()
+    from isaacsim.core.utils.extensions import enable_extension
+    enable_extension("omni.kit.asset_converter")
+
+    files = get_obj_files_in_dir(args.input_dir)
+    for file in files:
+        # Build paths
+        input_file_path = os.path.join(args.input_dir, file)
+        output_file_path = os.path.join(args.output_dir, file).replace(".obj", ".usd").replace(".OBJ", ".usd")
+
+        # Convert obj to usd
+        asyncio.get_event_loop().run_until_complete(convert(input_file_path, output_file_path))
+    
+    kit.close()
