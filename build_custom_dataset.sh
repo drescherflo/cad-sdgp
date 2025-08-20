f80f59d04e13e8c15ade8bf0bbb5146ac0dfc20f
Florian Drescher <floriandrescher@sundk.de>
2025-08-20 11:21:39 +0200

    refactor: restructure repository

--- build_custom_dataset.sh
+++ build_custom_dataset.sh
@@ -9,26 +9,26 @@ CUSTOM_DATASET_DIR="$HOME/custom_dataset"
 # Run dataset generation
 ## STEP to OBJ
 echo "Converting STEP files to OBJ"
-conda run --no-capture-output -n sodah-sdgp python step_to_obj_conversion/step_to_obj.py \
+conda run --no-capture-output -n sodah-sdgp python asset_converter/step_to_obj.py \
   --input_dir "$CUSTOM_DATASET_DIR/cad_models/step/" \
   --output_dir "$CUSTOM_DATASET_DIR/cad_models/obj/"
 
 ## OBJ to USD
 echo "Converting OBJ to USD"
-conda run --no-capture-output -n sodah-sdgp python obj_to_usd_conversion/obj_to_usd.py \
+conda run --no-capture-output -n sodah-sdgp python asset_converter/obj_to_usd.py \
   --input_dir "$CUSTOM_DATASET_DIR/cad_models/obj" \
   --output_dir "$CUSTOM_DATASET_DIR/cad_models/usd"
 
 ## Generate config for 6 DOF dataset
 echo "Generating config for the 6 DOF dataset generation"
-conda run --no-capture-output -n sodah-sdgp python custom_dataset/dataset_generation/config_generation/6_dof_config_generator.py \
+conda run --no-capture-output -n sodah-sdgp python dataset_config_generator/6_dof_config_generator.py \
    --usd_dir "$CUSTOM_DATASET_DIR/cad_models/usd/" \
    --out_path "$CUSTOM_DATASET_DIR/dataset_generator_configs/6_dof_generator_config.json" \
    --num_frames_per_object 1000
 
 ## Generate 6 DOF dataset
 echo "Generating 6 DOF dataset"
-conda run --no-capture-output -n sodah-sdgp python custom_dataset/dataset_generation/data_generation/6_dof_dataset_generator.py \
+conda run --no-capture-output -n sodah-sdgp python dataset_generator/6_dof_dataset_generator.py \
    --headless \
    --output_dir "$CUSTOM_DATASET_DIR/replicator_dataset/6_dof/" \
    --usd_dir "$CUSTOM_DATASET_DIR/cad_models/usd/" \
@@ -39,7 +39,7 @@ conda run --no-capture-output -n sodah-sdgp python custom_dataset/dataset_genera
 
 ## Generate config for conveyor dataset
 echo "Generating config for the conveyor belt dataset generation"
-conda run --no-capture-output -n sodah-sdgp python custom_dataset/dataset_generation/config_generation/conveyor_config_generator.py \
+conda run --no-capture-output -n sodah-sdgp python dataset_config_generator/conveyor_config_generator.py \
   --usd_dir "$CUSTOM_DATASET_DIR/cad_models/usd/" \
   --out_path "$CUSTOM_DATASET_DIR/dataset_generator_configs/conveyor_generator_config.json" \
   --object_init_min_x -6.0 \
@@ -51,7 +51,7 @@ conda run --no-capture-output -n sodah-sdgp python custom_dataset/dataset_genera
 
 ## Generate conveyor dataset
 echo "Generating the conveyor belt dataset"
-conda run --no-capture-output -n sodah-sdgp python custom_dataset/dataset_generation/data_generation/conveyor_belt_dataset_generator.py \
+conda run --no-capture-output -n sodah-sdgp python dataset_generator/conveyor_belt_dataset_generator.py \
   --headless \
   --output_dir "$CUSTOM_DATASET_DIR/replicator_dataset/conveyor_belt/" \
   --usd_dir "$CUSTOM_DATASET_DIR/cad_models/usd/" \
@@ -61,7 +61,7 @@ conda run --no-capture-output -n sodah-sdgp python custom_dataset/dataset_genera
 
 ## Convert dataset
 echo "Converting datasets to ROCA format"
-conda run --no-capture-output -n sodah-sdgp python custom_dataset/dataset_conversion/dataset_converter.py \
+conda run --no-capture-output -n sodah-sdgp python dataset_converter/dataset_converter.py \
   --obj_dir "$CUSTOM_DATASET_DIR/cad_models/obj/" \
   --replicator_data_dir "$CUSTOM_DATASET_DIR/replicator_dataset/6_dof/" \
   --replicator_data_dir "$CUSTOM_DATASET_DIR/replicator_dataset/conveyor_belt/" \
--- thesis/scripts/read_points_val.pkl.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-with open("points_val.pkl", "rb") as f:
-    data = pickle.load(f)
-print(data)
