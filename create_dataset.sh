81b3988c868ac77dd56f577b86dc062b776d3e2d
Phil Neuburger <ph121neu@htwg-konstanz.de>
2026-08-21 16:25:17 +0200

    added BOP Call

--- create_dataset.sh
+++ create_dataset.sh
@@ -74,10 +74,11 @@ pixi run python dataset_generator/conveyor_belt_dataset_generator.py \
   "${KEEP_CAD_MATERIALS_FLAG[@]}"
 
 ## Convert dataset
-echo "Converting datasets to ROCA format"
+echo "Converting datasets to ROCA and BOP format"
 pixi run python dataset_converter/dataset_converter.py \
   --obj_dir "$CUSTOM_DATASET_DIR/cad_models/obj/" \
   --replicator_data_dir "$CUSTOM_DATASET_DIR/replicator_dataset/6_dof/" \
   --replicator_data_dir "$CUSTOM_DATASET_DIR/replicator_dataset/conveyor_belt/" \
   --output_dir "$CUSTOM_DATASET_DIR/converted/" \
-  --converter ReplicatorToRoca
+  --converter ReplicatorToRoca \
+  --converter ReplicatorToBop
